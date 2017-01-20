f93f81bce8ecbeda2fbe15f430c98f942772ef7e
Skyler Rexroad <skyrex1095@gmail.com>
2017-01-19 22:55:11 -0700

    Added enemy size

--- enemy.lua
+++ enemy.lua
@@ -7,6 +7,8 @@ function Enemy:init(pos, img, moveSpeed, health)
 	self.moveSpeed = moveSpeed;
 	self.health = health
 
+	self.size = 16 -- TODO: Remove temp variable
+
 	self.inventory = {}
 end
 
@@ -16,5 +18,5 @@ end
 
 function Enemy:draw()
 	love.graphics.setColor(255, 0, 0, 255)
-	love.graphics.rectangle("fill", self.x, self.y, 32, 32) -- TODO: Draw img
+	love.graphics.rectangle("fill", self.x, self.y, self.size, self.size) -- TODO: Draw img
 end
